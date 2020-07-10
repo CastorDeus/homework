4c52a73544412c79399287e85a9da630f324978a
CastorDeus <58141326+CastorDeus@users.noreply.github.com>
2020-07-10 09:20:22 +0500

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,14 @@
+function fibo()
+{
+    var result = 0;
+    a = 1;
+    b = 1;
+    for(i = 3; i++; i<=n)
+    {
+        result = a + b;
+    }
+    return result;
+}
+var x = 0;
+x = fibo(3);
+document.write(x);
